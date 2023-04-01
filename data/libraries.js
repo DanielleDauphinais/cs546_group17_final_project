5903928d42f17b6f61f3a6ec0628b972225a71ad
Evan Jinks <evanjjinks@gmail.com>
2023-04-01 15:10:57 -0400

    Editing formatting to match main branch

--- data/libraries.js
+++ data/libraries.js
@@ -2,55 +2,52 @@ import {libraries} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 import validation from '../validation.js';
 
-const get = async (id) => {
-    // ejinks
-    id = validation.checkValidId(id, "Library ID");
-    const libraryCollection = await libraries();
-    let library = await libraryCollection.findOne({_id: new ObjectId(id)});
-    if (library === null) throw "Error: No library found with given ID.";
-    library._id = library._id.toString();
-    return library;
-}
+const exportedMethods = {
+    async get(id) {
+        // ejinks
+        id = validation.checkValidId(id, "Library ID");
+        const libraryCollection = await libraries();
+        let library = await libraryCollection.findOne({_id: new ObjectId(id)});
+        if (library === null) throw "Error: No library found with given ID.";
+        library._id = library._id.toString();
+        return library;
+    },
+    async getAllComments(id) {
+        // ejinks
+        id = validation.checkValidId(id, "Library ID");
+        const libraryCollection = await libraries();
+        let commentsList = await libraryCollection.findOne(
+            {_id: new ObjectId(id)},
+            {_id: 0, comments: 1}
+        );
+        if (commentsList == null) throw "Error: No library found with given ID.";
+        return commentsList;
+    },
+    async createComment(libraryId, userId, text) {
+        // ejinks
+        libraryId = validation.checkValidId(libraryId, "Library ID");
+        userId = validation.checkValidId(userId, "User ID");
 
-const getAllComments = async (id) => {
-    // ejinks
-    id = validation.checkValidId(id, "Library ID");
-    const libraryCollection = await libraries();
-    let commentsList = await libraryCollection.findOne(
-        {_id: new ObjectId(id)},
-        {_id: 0, comments: 1}
-    );
-    if (commentsList == null) throw "Error: No library found with given ID.";
-    return commentsList;
-}
+        text = validation.checkValidString(text, "Comment Body");
 
-const createComment = async (libraryId, userId, text) => {
-    // ejinks
-    libraryId = validation.checkValidId(libraryId, "Library ID");
-    userId = validation.checkValidId(userId, "User ID");
+        let newComment = {
+            _id: new ObjectId(),
+            userId: userId,
+            dateCreated: new Date().toLocaleDateString(),
+            text: text,
+            likes: []
+        }
 
-    text = validation.checkValidString(text, "Comment Body");
-
-    let newComment = {
-        _id: new ObjectId(),
-        userId: userId,
-        dateCreated: new Date().toLocaleDateString(),
-        text: text,
-        likes: []
+        const libraryCollection = await libraries();
+        await libraryCollection.updateOne(
+            {_id: new ObjectId(libraryId)},
+            {$push: {comments: newComment}}
+        );
     }
-
-    const libraryCollection = await libraries();
-    await libraryCollection.updateOne(
-        {_id: new ObjectId(libraryId)},
-        {$push: {comments: newComment}}
-    );
-}
-
 // getNumLikes
 // getNumFavorites
 // addLiketoComment
 // addLiketoLibrary
+}
 
-export default {
-    getAllComments
-};
+export default exportedMethods;
