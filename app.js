736e36392c1f43606aee55821b1a09c2ee758dba
vish <vishwanath5854@outlook.com>
2023-04-06 22:52:00 -0400

    File upload service is done.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from 'express';
 const app = express();
 import configRoutes from './routes/index.js';
+import morgan from "morgan";
 
 app.use(express.json());
+app.use(express.static('public'));
+
+/** 
+ * This is a small middleware which will give output the 
+ * summary of each request in the console, to understand
+ * what's happening. TLDR; It is a logger
+ */
+app.use(morgan("dev"));
 
 configRoutes(app);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import librariesRoutes from './libraries.js';
 import userRoutes from './users.js';
+import imageRouter from './image.js';
 
 const constructorMethod = (app) => {
   app.use('/libraries', librariesRoutes);
   app.use('/users', userRoutes);
+  app.use('/image', imageRouter);
 
   app.use('*', (req, res) => {
     res.status(404).json({error: 'Route Not found'});
