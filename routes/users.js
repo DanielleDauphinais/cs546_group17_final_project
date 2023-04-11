2aab799caed2c32e4f320f1ab1a60dd9f29eb304
George Alapatt <galapatt@stevens.edu>
2023-04-11 01:15:36 -0400

    Add error handlebar and handlebar helper functions

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,16 @@ import {userData} from '../data/index.js';
 import validation from '../validation.js';
 
 router
-  .route('/')
-  
-
-router
-  .route('/:id')
+  .route('/:id').get(async (req,res) => {
+    try {
+      var userId = validation.checkValidId(req.params.id, "User ID")
+    } catch (error) {
+      return res.status(404).render('error',
+      { searchValue:"user", 
+        errorCode:"404"
+      })
+    }
+  });
   
 
 export default router;
