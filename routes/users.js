1741ff9ca4c0a58b700988e288ee7970bb88556f
George Alapatt <galapatt@stevens.edu>
2023-04-12 12:06:52 -0400

    Created additional error checks for user search

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,39 @@
 import {Router} from 'express';
 const router = Router();
-import {userData} from '../data/index.js';
+import {userData,libraryData} from '../data/index.js';
 import validation from '../validation.js';
 
 router
   .route('/:id').get(async (req,res) => {
     try {
-      var userId = validation.checkValidId(req.params.id, "User ID")
+      var id = validation.checkValidId(req.params.id,"user_id");
+    } catch (error) {
+      return res.status(400).render('error',
+      { searchValue:"user", 
+        errorCode:"400"
+      })
+    }
+
+    try {
+      var user = await userData.getUserById(id)
     } catch (error) {
       return res.status(404).render('error',
       { searchValue:"user", 
         errorCode:"404"
+      })
+    }
+
+    try {
+      var favLibs = user.favLibraries.map(lib => libraryData.get(lib))
+      var ownedLibs = user.ownedLibraries.map(lib => libraryData.get(lib))
+      return res.status(200).render('user-profile',
+      { favLibs: favLibs,
+        ownedLibs: ownedLibs 
+      })
+    } catch (error) {
+      return res.status(500).render('error',
+      { searchValue:"user", 
+        errorCode:"500"
       })
     }
   });
