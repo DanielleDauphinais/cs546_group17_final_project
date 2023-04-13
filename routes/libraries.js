c2b3e9d0321f856e2a45bc456723562aee5a5f8d
DanielleDauphinais <djdauph@icloud.com>
2023-04-13 16:39:15 -0400

    Editted some style and create libary routes

--- routes/libraries.js
+++ routes/libraries.js
@@ -13,31 +13,55 @@ router
       res.status(500).json({error: e});
     }
   })
-  .post(async (req, res) => { // Currently creates libary and sents json of created library
+  .post(async (req, res) => { // TODO: Will need to edit for location and new servay
     const newLibraryData = req.body;
-    if (!newLibraryData || Object.keys(newLibraryData).length === 0) {
-      return res
-        .status(400)
-        .json({error: 'There are no fields in the request body'});
-    }
-    try { // NEED TO UNDATE FOR LOCATION AND IMAGE
+    let errors = [];
+    // TODO: NEED TO UNDATE FOR LOCATION AND IMAGE
+    try {
       newLibraryData.name = validation.checkString(newLibraryData.name, 'Name');
+    } catch (e) {
+      errors.push(e)
+    }
+    try {
       newLibraryData.ownerID = validation.checkId(newLibraryData.ownerID, "Library Owner ID");
+    } catch (e) {
+      errors.push(e)
+    }
+    try { // TODO: THIS WILL BE UPDATED BECAUSE THE WAY OF SERVY CHANGING
       newLibraryData.fullnessRating  = validation.isValidNumber(newLibraryData.fullnessRating, "Fullness Rating");
+    } catch (e) {
+      errors.push(e)
+    }
+    try { // TODO:THIS WILL BE UPDATED BECAUSE THE WAY OF SERVY CHANGING
       newLibraryData.genres = validation.checkStringArray(newLibraryData.genres, "Genres Available");
     } catch (e) {
-      return res.status(400).json({error: e});
+      errors.push(e)
     }
-
+    if (errors.length > 0) {
+      res.render('libraries/new', {
+        errors: errors,
+        hasErrors: true,
+        library: newLibraryData,
+        title: "Creating a Library", 
+        id: "NEED TO FIX"
+      });
+      return;
+    }
     try {
       const {name, ownerID, fullnessRating, genres} = newLibraryData;
       const newLibrary = await libraryData.create(name, location, image, ownerID, fullnessRating,genres);
-      res.json(newLibrary);
+      res.json(newLibrary); // TODO: will probably be to the library's page
     } catch (e) {
-      res.status(500).json({error: e});
+      res.status(500).render({errorCode:500, title:"error", id: "NEED TO FIX"});
     }
   });
 
+router
+  .route('/new')
+  .get(async (req, res) => {
+    // need to come back and fix the ID with cookie stuff
+    res.render('libraries/new', {title: "Creating a Library", id: "NEED TO FIX"});
+  });
 router
   .route('/:id')
   
