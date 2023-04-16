cbefe99483c6e67c0be3cf2d8b387075e5a3544a
Evan Jinks <evanjjinks@gmail.com>
2023-04-16 17:42:29 -0400

    Added library handlebar file and routes for the libraries and comments section. Also added a seed file for the database.

--- routes/libraries.js
+++ routes/libraries.js
@@ -5,7 +5,7 @@ import validation from '../validation.js';
 
 router
   .route('/')
-  .get(async (req, res) => { // Currently just sents json of all libraries
+  .get(async (req, res) => { // Currently just sends json of all libraries
     try {
       const libraryList = await libraryData.getAllLibraries();
       res.json(libraryList);
@@ -13,7 +13,7 @@ router
       res.status(500).json({error: e});
     }
   })
-  .post(async (req, res) => { // Currently creates libary and sents json of created library
+  .post(async (req, res) => { // Currently creates libary and sends json of created library
     const newLibraryData = req.body;
     if (!newLibraryData || Object.keys(newLibraryData).length === 0) {
       return res
@@ -57,6 +57,62 @@ router
       res.render('libraries/library', {title: library.name, library: library});
     } catch (e) {
       res.status(500).send(e);
+    }
+  });
+
+router
+  .route('/comments/create/:id')
+  .post(async (req, res) => {
+    //GET USER ID FROM MIDDLEWARE COOKIE
+    let id = req.params.id;
+    
+    id = validation.checkValidId(id);
+
+    let text = req.body.text;
+
+    text = validation.checkString(text);
+
+    let library;
+
+    try {
+      library = await libraryData.get(id);
+    } catch (e) {
+      return res.status(404).send("Error: No library with given ID");
+    }
+
+    try {
+      let createComment = await libraryData.createComment(id, userId, text); // Still need to get userid
+      res.redirect(`libraries/library/${id}`); //Do I need to rerender the page?
+    } catch (e) {
+      res.status(500).json({error: e});
+    }
+  });
+
+router
+  .route('/comments/like/:id')
+  .post(async (req, res) => {
+    //GET USER ID FROM MIDDLEWARE COOKIE
+    let id = req.params.id;
+    
+    id = validation.checkValidId(id);
+
+    let commentId = req.body.addCommentLike;
+
+    commentId = validation.checkString(commentId);
+
+    let library;
+
+    try {
+      library = await libraryData.get(id);
+    } catch (e) {
+      return res.status(404).send("Error: No library with given ID");
+    }
+
+    try {
+      let likeComment = await libraryData.likeComment(userid, commentId); // How to get comment id???
+      res.redirect(`libraries/library/${id}`);
+    } catch (e) {
+      res.status(500).json({error: e});
     }
   })
   
